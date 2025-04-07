f942ed2c15b51573306a2a30634e3213fc73c0c6
Bernardo jesus <140445825+abswing@users.noreply.github.com>
2025-04-07 20:32:26 -0300

    Implement Carro class and add salvar method in TelaCarro for vehicle data handling

--- aula04/TelaCarro.py
+++ aula04/TelaCarro.py
@@ -13,3 +13,15 @@ class TelaCarro (TelaVeiculo):
         self.txtPortas = QLineEdit(self) #QLineEdit recebe somente o que a própria classe recebe, por isto self
         self.layout.addWidget(self.lblPortas)
         self.layout.addWidget(self.txtPortas)
+
+
+    def salvar(self):
+        modelo = self.txtModelo.text() #modelo recebeu o QLineEdit, que é classe contendo um texto com as dimensões, com o final .text() eu estou afirmando que esta variável recebe somente a parte de texto da classe QLineEdit.
+        ano = self.txtAno.text() #mesma lógica.
+        if(ano != ""): #se o ano não for vazio,
+            ano = int(ano) #variável ano recebe o ano como um número inteiro,
+        portas = self.txtPortas.text()
+        if portas != "":
+            portas = int( portas )
+        carro = Carro(modelo, ano, portas) #variável carro recebe a classe Carro com as variáveis definidas na função (modelo, ano, portas) que são os parâmetros que ela normalmente recebe.
+        QMessageBox.information(self, "Carro salvo", str(carro) )
